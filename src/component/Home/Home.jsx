80cfe491060007d400d92823e4a12e92751abfc3
Bappy <bappymodok0011@gmail.com>
2023-11-29 12:14:34 +0600

    recat tost added

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -2,18 +2,26 @@ import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Tshart from '../Tshart/Tshart';
 import Cart from '../Cart/Cart';
+import toast from 'react-hot-toast';
 
 const Home = () => {
     const tsharts = useLoaderData()
-    const [cart,setCart] = useState([])
+    const [cart, setCart] = useState([])
     // console.log(cart);
 
-    const handelAddedCart = (tshart) =>{
-        const newCart = [...cart,tshart]
-        setCart(newCart)
+    const handelAddedCart = (tshart) => {
+        const exists = cart.find(ts => ts._id === tshart._id)
+        if (exists) {
+            toast.error("This product alredy added")
+        } else {
+            const newCart = [...cart, tshart]
+            setCart(newCart)
+            toast.success('Successfully product!')
+        }
+
     }
 
-    const handelRemove = (id) =>{
+    const handelRemove = (id) => {
         console.log(id);
         const reamingData = cart.filter(p => p._id !== id)
         setCart(reamingData);
@@ -24,17 +32,17 @@ const Home = () => {
         <div className='grid md:grid-cols-[3fr,1fr] mt-10 gap-5 px-3'>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    tsharts.map(tshart => <Tshart 
-                    key={tshart._id}
-                    tshart={tshart}
-                    handelAddedCart={handelAddedCart}
+                    tsharts.map(tshart => <Tshart
+                        key={tshart._id}
+                        tshart={tshart}
+                        handelAddedCart={handelAddedCart}
                     ></Tshart>)
                 }
             </div>
             <div>
-                <Cart 
-                cart={cart}
-                handelRemove={handelRemove}
+                <Cart
+                    cart={cart}
+                    handelRemove={handelRemove}
                 ></Cart>
             </div>
         </div>
--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import  { Toaster } from 'react-hot-toast';
 
 const Layout = () => {
     return (
         <div className='max-w-[1240px] mx-auto'>
             <Header></Header>
             <Outlet></Outlet>
+            <Toaster />
         </div>
     );
 };
